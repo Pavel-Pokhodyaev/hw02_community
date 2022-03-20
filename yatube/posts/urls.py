706a62e9869c39ddf5a427bfbbbda9ec4c971cfa
Pavel-Pokhodyaev <pavka_91@mail.ru>
2022-03-20 17:16:44 +0300

    first

---
+++ yatube/posts/urls.py
@@ -0,0 +1,14 @@
+# posts/urls.py
+from django.urls import path
+from . import views
+
+app_name = 'posts'
+
+urlpatterns = [
+    # Главная страница
+    path('', views.index, name ='index'),
+    # Страница со списком постов
+    path('index.html', views.index),
+    path('group/<slug:slug>/', views.group_posts, name='group_list'),
+    path('group_list/<slug:slug>', views.group_posts, name='group_list')
+] 
